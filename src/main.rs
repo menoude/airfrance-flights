fd69eb861f32a1f5600242ea6acf80344223c9b5
menoude <mennadyami@hotmail.com>
2021-07-22 17:03:33 +0200

    Schedule

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,26 @@
 use airfrance::*;
+use {
+    clokwerk::{Scheduler, TimeUnits},
+    std::{thread, time::Duration},
+};
 
 fn main() {
     let client = reqwest::blocking::Client::new();
     let headers = build_headers_map();
     let date = "2021-07-28";
-    println!(
-        "For {} -> {}",
-        date,
-        match date_availability(&client, headers, date) {
-            false => "No flight available",
-            true => "Some flight available",
-        }
-    );
+    let mut scheduler = Scheduler::new();
+    scheduler.every(1.minutes()).run(move || {
+        println!(
+            "For {} -> {}",
+            date,
+            match date_availability(&client, headers.clone(), date) {
+                false => "No flight available",
+                true => "Some flight available",
+            }
+        )
+    });
+    loop {
+        scheduler.run_pending();
+        thread::sleep(Duration::from_millis(5000));
+    }
 }
