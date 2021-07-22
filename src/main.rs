52c1592ad98ecf51ff41aed85312cbcfb3c7d730
menoude <mennadyami@hotmail.com>
2021-07-22 17:30:43 +0200

    Time and text color

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,31 @@ use airfrance::*;
 use {
     clokwerk::{Scheduler, TimeUnits},
     std::{thread, time::Duration},
+    termion::color,
 };
 
 fn main() {
     let client = reqwest::blocking::Client::new();
     let headers = build_headers_map();
     let date = "2021-07-28";
+    println!("For {}", date);
     let mut scheduler = Scheduler::new();
     scheduler.every(1.minutes()).run(move || {
-        println!(
-            "For {} -> {}",
-            date,
-            match date_availability(&client, headers.clone(), date) {
-                false => "No flight available",
-                true => "Some flight available",
-            }
-        )
+        let time = chrono::Local::now().time().format("%H:%M:%S").to_string();
+        match date_availability(&client, headers.clone(), date) {
+            false => println!(
+                "{}{} | No flight available.{}",
+                color::Fg(color::Red),
+                time,
+                color::Fg(color::Reset)
+            ),
+            true => println!(
+                "{}{} | Some flight available!!!!!!!!!!!{}",
+                color::Fg(color::Green),
+                time,
+                color::Fg(color::Reset)
+            ),
+        }
     });
     loop {
         scheduler.run_pending();
